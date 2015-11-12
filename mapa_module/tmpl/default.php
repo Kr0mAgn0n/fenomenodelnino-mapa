54dac6d48a091bbba9654a96208dd977de4bc1a6
Mario Lamas <mario@localhost.localdomain>
2015-11-12 17:07:37 -0500

    Avance del módulo de Mapa de Alertas

--- mapa_module/tmpl/default.php
+++ mapa_module/tmpl/default.php
@@ -10,6 +10,23 @@ defined('_JEXEC') or die; ?>
     width: 100%;
     height: 450px;
 }
+
+.alertas {
+    width: 100%;
+}
+
+.alertas td {
+    border: 1px solid #000;
+}
+
+#tabs {
+    height: 240px;
+}
+
+#tabs .contenido {
+    height: 170px;
+    overflow-y: auto;
+}
 </style>
 
 <div class="row-fluid">
@@ -28,21 +45,22 @@ defined('_JEXEC') or die; ?>
             </ul> 
             
             <?php foreach ($areas as $area) { ?>
-                <div id="<?php echo $area->area; ?>">
+                <div id="<?php echo $area->area; ?>" class="contenido">
                     
-                    <table>
+                    <table class="alertas">
                         <tr>
+                            <th>Título</th>
                             <th>Latitud</th>
                             <th>Longitud</th>
-                            <th>Título</th>
+                            
                         </tr>
                         
                         <?php foreach ($alertas as $alerta) { ?>
                             <?php if ($alerta->area_name == $area->area) { ?>
                                 <tr>
-                                    <td><?php echo $alerta->latitud; ?></td>
-                                    <td><?php echo $alerta->longitud; ?></td>
                                     <td><?php echo $alerta->titulo; ?></td>
+                                    <td><?php echo $alerta->latitud; ?></td>
+                                    <td><?php echo $alerta->longitud; ?></td>                                    
                                 </tr>
                             <?php } ?>
                             
@@ -54,6 +72,16 @@ defined('_JEXEC') or die; ?>
             <?php } ?>
             
         </div>
+        
+        <div id="leyenda">
+        
+        <h3>Leyenda</h3>
+        
+        <p><img src="/modules/mod_mapa/images/pin_azul.png" /> Alerta Oceanográfica</p>
+        <p><img src="/modules/mod_mapa/images/pin_amarillo.png" /> Alerta Hidrológica</p>
+        <p><img src="/modules/mod_mapa/images/pin_rojo.png" /> Alerta Sanitaria</p>
+        
+        </div>
 
 
     </div>
@@ -64,29 +92,64 @@ defined('_JEXEC') or die; ?>
 <script type="text/javascript">
 
 var map;
+
+var alertas = [];
+
+<?php foreach ($alertas as $alerta) { ?>
+    alertas.push({area: "<?php echo $alerta->area_name; ?>", titulo: "<?php echo $alerta->titulo; ?>", latitud: <?php echo $alerta->latitud; ?>, longitud: <?php echo $alerta->longitud; ?>, descripcion: "<?php echo $alerta->descripcion ?>"});
+<?php } ?> 
+
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -10, lng: -76},
         zoom: 5
         });
 
-    var myLatLng = {lat: -10, lng: -76};
-
-    var marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        title: 'Hello World!'
+    var markers = [];
+    var infowindows = [];
+    
+    jQuery.each(alertas, function(index, alerta) {
+        var myLatLng = {lat: alerta.latitud, lng: alerta.longitud}
+        
+        if (alerta.area == "Oceanografía")
+            var image = "/modules/mod_mapa/images/pin_azul.png"
+        
+        if (alerta.area == "Hidrología")
+            var image = "/modules/mod_mapa/images/pin_amarillo.png"
+            
+        if (alerta.area == "Sanitaria")
+            var image = "/modules/mod_mapa/images/pin_rojo.png"
+        
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map,
+            title: alerta.titulo,
+            icon: image
+        });
+        markers.push(marker);
+        
+        var infowindow = new google.maps.InfoWindow({
+            content: "<h3>" + alerta.titulo + "</h3>" + "<p>" + alerta.descripcion + "</p>"
+        });
+        infowindows.push(infowindow);
+        
     });
 
-    var contentString = "<h3>Este es el título de la alerta</h3>"+
-        "<p>Esta es la descripción de la alerta.</p>"
-
-    var infowindow = new google.maps.InfoWindow({
-        content: contentString
+    jQuery.each(markers, function(index, marker) {
+        marker.addListener('click', function() {
+            
+            closeInfoWindows(infowindows);
+            
+            infowindows[index].open(map, marker);
+        });    
     });
 
-    marker.addListener('click', function() {
-        infowindow.open(map, marker);
+}
+
+function closeInfoWindows (infowindows) {
+
+    jQuery.each(infowindows, function(index, infowindow) {
+        infowindow.close();
     });
 
 }
@@ -101,7 +164,3 @@ window.addEvent('domready', function(){
 
 <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDGptWzvTyBnAeCuOVDkBPTZO1lpSsme1I&callback=initMap">
 </script>
-
-<!-- <script src="/modules/mod_mapa/js/rotater.js"></script> -->
-<!-- <script src="/modules/mod_mapa/js/tabs.js"></script> -->
-
